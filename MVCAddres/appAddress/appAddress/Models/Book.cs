68d35b1071a1746055a6743061278f3893c27712
bruno <brunoivanzaratefernandez@gmail.com>
2019-10-05 18:16:09 -0300

    Avance Xamarin

---
+++ MVCAddres/appAddress/appAddress/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace appAddress.Models
+{
+
+
+    public enum TypeContact
+    {
+        Telephone,
+        Email,
+        Facebook,
+        Twitter,
+        Instagram,
+        Dblink,
+    }
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Name { get; set; }
+        public TypeContact Type { get; set; }
+        public string Contact { get; set; }
+    }
+}
---
+++ MVCAddres/appAddress/appAddress/Models/KeyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace appAddress.Models
+{
+    internal class KeyAttribute : Attribute
+    {
+    }
+}
